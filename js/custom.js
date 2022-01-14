df2948d77cfb566675392fe875969ee7c7eab31f
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2022-01-14 10:18:16 +0500

    test
    test

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+(function ($) {
+
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+})(jQuery);
